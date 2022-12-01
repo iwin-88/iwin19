4ef7e60845e57f7f638b341b84c584839d8b89fd
Yat Sokngam <itknowledge321@gmail.com>
2022-12-01 15:55:15 +0700

    iwin19

---
+++ js.js
@@ -0,0 +1,27 @@
+function openNav(){
+    document.getElementById('menu').style.width = '60%';
+}
+function closeNav(){
+    document.getElementById('menu').style.width = '0%';
+}
+
+// document.onkeydown = function(e)
+// {
+//     if(event.keyCode == 123)
+//     {
+//         return false;
+//     }
+//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
+//     {
+//         return false;
+//     }
+//     if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
+//     {
+//         return false;
+//     }
+//     if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+//     {
+//         return false;
+//     }
+
+// }
